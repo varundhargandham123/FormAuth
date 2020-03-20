9d77ba59ef7fb29c81d0992f6f34a3deecee6cdd
Varundhar_Gandham <Varundhar_Gandham@EPINHYDW013A>
2020-03-20 16:17:52 +0530

    Add project files.

---
+++ FormAuth/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace FormAuth.Controllers
+{/// <summary>
+/// This Controller has Login and Success actions
+/// </summary>
+    public class UserController : Controller
+    {/// <summary>
+    /// This action is used to get the details
+    /// </summary>
+    /// <returns></returns>
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+        /// <summary>
+        /// This action is to post the details
+        /// </summary>
+        /// <param name="formCollection"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Login(FormCollection formCollection)
+        {
+            if (formCollection.Get("UserName") == "Varun" && formCollection.Get("Password") == "Varun")
+            {
+                return View("Success");
+            }
+            else if (formCollection.Get("UserName") == "Admin" && formCollection.Get("Password") == "Admin")
+            {
+                return View("Success");
+            }
+            else if (formCollection.Get("UserName") == "User" && formCollection.Get("Password") == "User")
+            {
+                return View("Success");
+            }
+
+            return View("Login");
+        }/// <summary>
+        /// this action is to dispaly the success login
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        public ActionResult Success()
+        {
+            return View();
+        }
+    }
+}
---
+++ FormAuth/Models/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FormAuth.Models
+{
+    public class LoginModel
+    {
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
